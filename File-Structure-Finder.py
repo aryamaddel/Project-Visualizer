3f453dddde5308786448944bbb758b317bf4289d
Arya Maddel <maddelarya@gmail.com>
2023-06-18 23:12:02 +0530

    level 0 specific

--- File-Structure-Finder.py
+++ File-Structure-Finder.py
@@ -4,12 +4,18 @@ def print_directory_contents(path, level=0, exclude_dirs=[]):
     for child in os.listdir(path):
         child_path = os.path.join(path, child)
         if os.path.isdir(child_path):
-            print('    ' * level + child)
+            if level == 0:
+                print('    ' * level + child)
+            else:
+                print('    ' * level + '|-' + child)
             if child not in exclude_dirs:
                 print_directory_contents(child_path, level + 1, exclude_dirs)
         else:
-            print('    ' * level + '|-' + os.path.basename(child_path))
+            if level == 0:
+                print('    ' * level + os.path.basename(child_path))
+            else:
+                print('    ' * level + '|-' + os.path.basename(child_path))
 
-folder_path = "C:/Users/Arya/Desktop/New folder/my-app"
+folder_path = "C:/Users/Arya/Github/Tatsumaki-Bot"
 exclude_dirs = ['node_modules', 'venv', '.git', '__pycache__']
 print_directory_contents(folder_path, 0, exclude_dirs)
