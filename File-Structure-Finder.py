9cabd88491dc6fce90944ab2471dad1235f75468
Arya Maddel <maddelarya@gmail.com>
2023-06-19 00:17:16 +0530

    Add copy to clipboard button

--- File-Structure-Finder.py
+++ File-Structure-Finder.py
@@ -17,10 +17,17 @@ def print_directory_contents(path, level=0, exclude_dirs=[]):
 
 def select_folder():
     folder_path = filedialog.askdirectory()
-    exclude_dirs = ['node_modules', 'venv', '.venv', '.git', '__pycache__']
-    text = print_directory_contents(folder_path, 0, exclude_dirs)
-    text_box.delete('1.0', tk.END)
-    text_box.insert(tk.END, text)
+    if folder_path:
+        exclude_dirs = ['node_modules', 'venv', '.venv', '.git', '__pycache__']
+        text = print_directory_contents(folder_path, 0, exclude_dirs)
+        text_box.delete('1.0', tk.END)
+        text_box.insert(tk.END, text)
+        copy_button.pack()
+
+
+def copy_to_clipboard():
+    root.clipboard_clear()
+    root.clipboard_append(text_box.get("1.0", tk.END))
 
 
 root = tk.Tk()
@@ -31,5 +38,8 @@ text_box.pack()
 
 select_button = tk.Button(root, text="Select Folder", command=select_folder)
 select_button.pack()
+
+copy_button = tk.Button(root, text="Copy to Clipboard",
+                        command=copy_to_clipboard)
 
 root.mainloop()
