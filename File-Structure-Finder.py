0aeb526fb918f87424a9ff12d7dc6797ced8b9c5
Arya Maddel <maddelarya@gmail.com>
2023-06-18 23:21:37 +0530

    used a prefix variable

--- File-Structure-Finder.py
+++ File-Structure-Finder.py
@@ -3,18 +3,10 @@ import os
 def print_directory_contents(path, level=0, exclude_dirs=[]):
     for child in os.listdir(path):
         child_path = os.path.join(path, child)
-        if os.path.isdir(child_path):
-            if level == 0:
-                print('    ' * level + child)
-            else:
-                print('    ' * level + '|-' + child)
-            if child not in exclude_dirs:
-                print_directory_contents(child_path, level + 1, exclude_dirs)
-        else:
-            if level == 0:
-                print('    ' * level + os.path.basename(child_path))
-            else:
-                print('    ' * level + '|-' + os.path.basename(child_path))
+        prefix = '|-' if level > 0 else ''
+        print('    ' * level + prefix + child)
+        if os.path.isdir(child_path) and child not in exclude_dirs:
+            print_directory_contents(child_path, level + 1, exclude_dirs)
 
 folder_path = "C:/Users/Arya/Github/Tatsumaki-Bot"
 exclude_dirs = ['node_modules', 'venv', '.git', '__pycache__']
